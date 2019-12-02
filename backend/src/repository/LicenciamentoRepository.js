5e9453a223bfdb332d0f01f283c9ae832de4d10c
Danilo Marques <danilomarques20@hotmail.com>
2019-12-02 18:22:32 -0300

    Ajustando o nome dos atributos nos triggers

--- backend/src/repository/LicenciamentoRepository.js
+++ backend/src/repository/LicenciamentoRepository.js
@@ -3,7 +3,7 @@ const pool = require('../config/connection');
 class LicenciamentoRepository {
 	static async findAll() {
 		try {
-			const result = await pool.query('SELECT * FROM licenciamento');
+			const result = await pool.query('SELECT * FROM licenciamento order by(renavam)');
 			return result.rows;
 		} catch(e) {
 			throw e;
